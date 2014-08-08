e9b80f8f781a3ded40b0698b3bc825aec59b7e04
Ethan Gould <ethan.g.907@gmail.com>
2014-08-07 23:33:02 -0400

    added backgroundcolor changer function

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 $(document).ready(function(){
   $(".bottom").on("click", fadeColor)
-
+  $(".col-2").on("click", backgroundMix)
 });
 
 var fadeColor = function(){
   $(".small-bottom").toggle( "highlight" );
+}
+
+var backgroundMix = function(){
+  $(".col-2").toggleClass('turnup')
 }
